359a728a1b396e232499ec851aa790ed5b294a99
Soybeany <soybeany@qq.com>
2022-02-10 15:06:20 +0800

    StorageBuilder增加ttl与ttlErr

--- bd-cachev2/src/main/java/com/soybeany/cache/v2/storage/StdStorageBuilder.java
+++ bd-cachev2/src/main/java/com/soybeany/cache/v2/storage/StdStorageBuilder.java
@@ -12,16 +12,28 @@ import lombok.experimental.Accessors;
 public abstract class StdStorageBuilder<Param, Data> {
 
     /**
-     * 正常数据的生存时间，用于一般场景
+     * 正常数据的生存时间，用于一般场景(单位：秒)，与{@link #pTtl}同时设置时，以{@link #pTtl}为准
      */
     @Setter
-    protected int pTtl = Integer.MAX_VALUE;
+    private int ttl = Integer.MAX_VALUE / 1000 - 1;
 
     /**
-     * 异常的生存时间，用于防缓存穿透等场景
+     * 异常的生存时间，用于防缓存穿透等场景(单位：秒)，与{@link #pTtlErr}同时设置时，以{@link #pTtlErr}为准
      */
     @Setter
-    protected int pTtlErr = 60000;
+    private int ttlErr = 60;
+
+    /**
+     * 正常数据的生存时间，用于一般场景(单位：毫秒)，与{@link #ttl}同时设置时，以此为准
+     */
+    @Setter
+    protected int pTtl;
+
+    /**
+     * 异常的生存时间，用于防缓存穿透等场景(单位：毫秒)，与{@link #ttlErr}同时设置时，以此为准
+     */
+    @Setter
+    protected int pTtlErr;
 
     public ICacheStorage<Param, Data> build() {
         // 预处理时间
@@ -37,6 +49,14 @@ public abstract class StdStorageBuilder<Param, Data> {
     // ***********************内部方法****************************
 
     private void handleTtl() {
+        // 整合
+        if (pTtl == 0) {
+            pTtl = ttl * 1000;
+        }
+        if (pTtlErr == 0) {
+            pTtlErr = ttlErr * 1000;
+        }
+        // 微调
         pTtl = Math.max(pTtl, 1);
         pTtlErr = Math.max(pTtlErr, 1);
         if (pTtlErr > pTtl) {
